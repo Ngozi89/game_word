283931d1d26f02c37ecdca624d4c06ff981990d2
Ngozi Omenka <ngozi.madu@live.se>
2024-07-23 07:52:58 +0000

    Added game rules and player name

--- run.py
+++ run.py
@@ -2,9 +2,37 @@
 import random
 
 #Add player choice
-print("Welcome game world")
+print("Welcome to game world")
+
+player = input("Do you want to play Rock, Paper, Scissors game?")
+
+if player.lower() != "yes":
+    quit()
+else:
+    print("Here is the rules for the game ")  
+    print(" . There are 5 choices to chose (Fire, Wave, Fish, Boat, Grass)")
+    print()
+    print(" . You chose one and computer chose one")
+    print()
+    print(" . If your choice is same as the computer, you lose.")
+    print()
+    print(" . If your choice beats computer chose, you wine if not you lose")
+    print()
+
+player = input(" . If you are ready, type 'ok' ")
+
+if player.lower() != "ok":
+    quit()
+else:
+    print("Okay, lets' go :)")
+print()   
+# Let player enter name
+name = input("Enter your name ").capitalize()
+"Name: "
+print("Hi,", str(name) + "!")
+
 start = input("press enter to start: ")
-playerchoice = input("What type of Pokemon do you chose? (Fire, Wave, Fish, Boat, Grass) ").lower()
+playerchoice = input("Make a choice (Fire, Wave, Fish, Boat, Grass) ").lower()
 if playerchoice == "fire":
     print('''You chose '''+playerchoice+'''!''') #Print player choice
     print('''⠀⠀⠀⠀⠀⠀⢱⣆⠀⠀⠀⠀⠀⠀
